e4e96c3c3c4dabb41d409d5d9f29c6151cdfe946
hnm938 <mrbooomboom@gmail.com>
2023-10-19 17:15:51 -0400

    fixed auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,52 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { getServerSession } from "next-auth";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb";
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { getServerSession } from "next-auth";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
+// import clientPromise from "../../../lib/mongodb";
 
-const adminEmails = ["hnm938@gmail.com", "abrahamhodos@gmail.com"];
+// const adminEmails = ["hnm938@gmail.com", "abrahamhodos@gmail.com"];
+
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//   ],
+//   adapter: MongoDBAdapter(clientPromise),
+//   callbacks: {
+//     signIn: async (session, token, user) => {
+//       if (adminEmails.includes(session?.user?.email)) {
+//         return session;
+//       } else {
+//         return false;
+//       }
+//     },
+//   },
+// };
+
+// export default NextAuth(authOptions);
+
+// export async function isAdminRequest(req, res) {
+//   const session = await getServerSession(req, res, authOptions);
+//   if (!adminEmails.includes(session?.user?.email)) {
+//     res.status(403);
+//     res.end();
+//     throw "Access Denied";
+//   }
+// }
+
+import NextAuth, { getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import { Admin } from "@/models/Admin";
+import { mongooseConnect } from "@/lib/mongoose";
+
+async function isAdminEmail(email) {
+  mongooseConnect();
+  return !!(await Admin.findOne({ email }));
+}
 
 export const authOptions = {
   providers: [
@@ -15,8 +57,8 @@ export const authOptions = {
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    signIn: async (session, token, user) => {
-      if (adminEmails.includes(session?.user?.email)) {
+    session: async ({ session, token, user }) => {
+      if (await isAdminEmail(session?.user?.email)) {
         return session;
       } else {
         return false;
@@ -29,9 +71,9 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(403);
+  if (!(await isAdminEmail(session?.user?.email))) {
+    res.status(401);
     res.end();
-    throw "Access Denied";
+    throw "not an admin";
   }
 }
